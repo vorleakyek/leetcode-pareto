d4d915078b6831fd7f045eb962825a5375f4262d
Vorleak Yek <vorleakyek@gmail.com>
2024-12-09 20:53:18 -0800

    update to obtain O(n) time complexity

--- contains-duplicate-II.js
+++ contains-duplicate-II.js
@@ -5,6 +5,10 @@
  * @param {number} k
  * @return {boolean}
  */
+
+// Way #1
+// Time: O(n*k)
+// Space: O(1)
 var containsNearbyDuplicate = function (nums, k) {
   for (let i = 0; i <= nums.length - 2; i++) {
     for (let j = i + 1; j <= i + k; j++) {
@@ -12,6 +16,29 @@ var containsNearbyDuplicate = function (nums, k) {
         if (nums[i] === nums[j]) {
           return true
         }
+      }
+    }
+  }
+  return false;
+};
+
+
+// Way #2 - using Hash Table
+// Time: O(n)
+// Space: O(n)
+// ref: https://www.algo.monster/liteproblems/219
+
+var containsNearbyDuplicate = function (nums, k) {
+  const indexMap = new Map(); // {}
+
+  for (let ind = 0; ind < nums.length; ind++) {
+    if (!indexMap.has(nums[ind])) {
+      indexMap.set(nums[ind], ind);
+    } else {
+      if (ind - indexMap.get(nums[ind]) > k) {
+        indexMap.set(nums[ind], ind);
+      } else {
+        return true
       }
     }
   }
